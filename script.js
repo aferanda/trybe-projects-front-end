9339dfc01b6e1a0bcfb2007f4255160060be3034
Amanda Fernandes <aferanda@gmail.com>
2021-08-25 15:55:09 -0300

    Add requisito 5

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const buttonLogin = document.getElementById("button-login");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-
-
+const buttonLogin = document.getElementById('button-login');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
 
 /* Função butão de login */
-buttonLogin.addEventListener('click', btnLogin);
-function btnLogin () {
-    let valorEmail = email.value;
-    let valorSenha = senha.value;
-    if ( valorEmail === 'tryber@teste.com' && valorSenha === '123456') {
-        alert("Olá, Tryber!");
-    } else {
-        alert("Email ou senha inválidos.");
-    }
-    
+function btnLogin() {
+  const valorEmail = email.value;
+  const valorSenha = senha.value;
+  if (valorEmail === 'tryber@teste.com' && valorSenha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
+
+buttonLogin.addEventListener('click', btnLogin);
