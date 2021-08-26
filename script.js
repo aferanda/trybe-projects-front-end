59dbec90ee95dcbbd7ed1ec79b850e586ba81d4e
Amanda Fernandes <aferanda@gmail.com>
2021-08-26 15:58:19 -0300

    Inicia requisito 21

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const agreed = document.getElementById('agreement');
 const button = document.getElementById('submit-btn');
-const textArea = document.getElementById('textarea');
 const count = document.getElementById('counter');
+
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputHouse = document.getElementById('house');
+const inputFamily = document.getElementsByName('family');
+const inputSubjects = document.getElementsByClassName('subject');
+const inputRate = document.getElementsByName('rate');
+const textArea = document.getElementById('textarea');
 
 /* Função botão de login */
 function btnLogin() {
@@ -34,6 +42,36 @@ function countChars() {
   }
   count.innerText = `${restante} Restantes`;
 }
+
+function fullName() {
+  const inputFullName = `Name: ${inputName.value} ${inputLastName.value}`;
+  return inputFullName;
+}
+
+function emailForm() {
+  const emailValue = `Email: ${inputEmail.value}`;
+  return emailValue;
+}
+
+function houseForm() {
+  const houseValue = `Casa: ${inputHouse.value}`;
+  return houseValue;
+}
+
+function familyForm() {
+  let familyValue = '';
+  for (let index = 0; index < inputFamily.length; index += 1) {
+    if (inputFamily[index].checked) {
+      familyValue = `Família: ${inputFamily[index].value}`;
+    }
+  }
+  return familyValue;
+}
+
+fullName();
+emailForm();
+houseForm();
+familyForm();
 
 buttonLogin.addEventListener('click', btnLogin);
 agreed.addEventListener('change', habilitaBtn);
