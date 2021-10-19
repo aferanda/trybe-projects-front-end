51edabfed7e112c66b1850de6415683a24513f94
Amanda Fernandes <aferanda@gmail.com>
2021-10-19 15:04:52 -0300

    Inicia requisito 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
         </Switch>
       </BrowserRouter>
     );
