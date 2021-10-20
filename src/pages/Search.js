06c192b6e421ed0a3b017161d7a911c61dab48aa
Amanda Fernandes <aferanda@gmail.com>
2021-10-20 10:36:17 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      minLength: 2,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { name, minLength } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist-input">
+            <input
+              name="name"
+              value={ name }
+              onChange={ this.onInputChange }
+              placeholder="Nome do Artista"
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ name.length < minLength }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
