f722d96b98db573887da516a364a36179c8cfca4
Amanda Fernandes <aferanda@gmail.com>
2021-10-20 17:20:19 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: '',
+      artist: '',
+      artistSearch: '',
       minLength: 2,
+      loading: false,
+      resolve: false,
+      albums: [],
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onClickButton = this.onClickButton.bind(this);
+    this.renderAlbums = this.renderAlbums.bind(this);
   }
 
   onInputChange({ target }) {
@@ -20,16 +29,60 @@ class Search extends Component {
     });
   }
 
+  async onClickButton(artistName) {
+    const { artist } = this.state;
+    this.setState({
+      loading: true,
+      artistSearch: artist,
+    });
+    const api = await searchAlbumsAPI(artistName);
+    this.setState({
+      artist: '',
+      albums: api,
+      loading: false,
+      resolve: true,
+    });
+  }
+
+  renderAlbums() {
+    const { albums, resolve, artistSearch } = this.state;
+    if (resolve && albums.length === 0) {
+      return <h2>Nenhum álbum foi encontrado</h2>;
+    }
+    if (resolve) {
+      return (
+        <div>
+          <h2>{`Resultado de álbuns de: ${artistSearch}`}</h2>
+          <section>
+            { albums.map((album, index) => (
+              <div key={ album.collectionId }>
+                <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                <p>{ album.collectionName }</p>
+                <Link
+                  to={ `/album/${album.collectionId}` }
+                  data-testid={ `link-to-album-${album.collectionId}` }
+                >
+                  { `Album ${index + 1}` }
+                </Link>
+              </div>
+            ))}
+          </section>
+        </div>
+      );
+    }
+  }
+
   render() {
-    const { name, minLength } = this.state;
+    const { artist, minLength, loading } = this.state;
     return (
       <div data-testid="page-search">
+        { loading && <Loading /> }
         <Header />
         <form>
           <label htmlFor="search-artist-input">
             <input
-              name="name"
-              value={ name }
+              name="artist"
+              value={ artist }
               onChange={ this.onInputChange }
               placeholder="Nome do Artista"
               data-testid="search-artist-input"
@@ -37,11 +90,13 @@ class Search extends Component {
           </label>
           <button
             type="button"
-            disabled={ name.length < minLength }
+            onClick={ () => this.onClickButton(artist) }
+            disabled={ artist.length < minLength }
             data-testid="search-artist-button"
           >
             Pesquisar
           </button>
+          { this.renderAlbums() }
         </form>
       </div>
     );
