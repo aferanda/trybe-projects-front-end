6f99d9c7dc7e16cb7c935b8b43a2d079c4c7430c
Amanda Fernandes <aferanda@gmail.com>
2021-11-19 15:30:48 -0300

    configura redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const USER = 'USER';
+export const WALLET = 'WALLET';
+
+export const user = (payload) => ({
+  type: USER,
+  payload,
+});
+
+export const wallet = (payload) => ({
+  type: WALLET,
+  payload,
+});
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET:
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
