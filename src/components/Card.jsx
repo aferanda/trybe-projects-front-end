897f0cb6153ca4a37d02544f4b5337e15134b2c5
Amanda Fernandes <aferanda@gmail.com>
2021-12-02 17:09:07 -0300

    style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/card.css';
 
 class Card extends Component {
   render() {
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/form.css';
 
 class Form extends Component {
   render() {
